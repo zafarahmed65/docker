d493e92a1d50f90a3ccd1900afa1212ad60018db
dheeraj <dheeraj.br2@gmail.com>
2023-09-17 22:14:10 +0530

    update server.js with better console message

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ import app from './app.js';
 
 const PORT = process.env.PORT;
 app.listen(PORT, () =>
-  console.log(`Server running on port ${PORT}`, process.env.NODE_ENV)
+  console.log(`Server running on ${process.env.NODE_ENV}:`, PORT)
 );
